0ead992d70e3352511f60244d02c69c2fc22bcf4
Aditya Das <adityabkp1234@gmail.com>
2022-07-03 01:01:51 +0530

    mongodb with mongoose midway 🌱

--- models/tourModel.js
+++ models/tourModel.js
@@ -49,6 +49,7 @@ const tourSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now(),
+    select: false, // To Hide Sensitive Data In Output
   },
   startDates: [Date],
 });
