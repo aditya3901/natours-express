5dcdf9993742d511edc323c1f6d4ddd5f24f29f3
Aditya Das <adityabkp1234@gmail.com>
2022-07-21 10:26:13 +0530

    auth, security done

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -8,13 +13,50 @@ const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
 
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!',
+});
+app.use('/api/v1/users', limiter);
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Body parser, reading data from body into req.body
 app.use(express.json());
+
+// Data sanitization against NoSQL query injection
+// eg: LOGIN => { "email": { "$gt": "" }, "password": "test123" }
+app.use(mongoSanitize());
+
+// Data sanitization against XSS (cross site scripting) attack
+// eg: SIGNUP => { "email": "test@gmail.com", "password": "test123", "name": "<div id='some-bad-stuff'>Name</div>" }
+app.use(xss());
+
+// Prevent Parameter Pollution, hpp => HTTP parameter pollution
+// eg: Query => {{URL}}api/v1/tours?sort=duration&sort=price&sort=createdAt
+app.use(
+  hpp({
+    // ALLOWED => {{URL}}api/v1/tours?duration=5&duration=9
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'difficulty',
+      'price',
+      'maxGroupSize',
+    ],
+  })
+);
 
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
